2959f80f15468d748fcbe133378d51295eb5a920
Tai Silva30 <tatihsilva16@gmail.com>
2024-09-05 20:52:46 -0300

    adicionando arquivo

---
+++ aula.py
@@ -0,0 +1,16 @@
+texto = "ola, mundo"
+
+#fatiar do inicio até um indice especifico
+print(texto[:5])
+
+#fatiar a partir de um indice especifico até o final
+print(texto[6:])
+
+#fatiar do inicio até o final, com um passo especifico
+print(texto[::2])
+
+#fatiar com inicioe e fim especifico
+print(texto[4:9])
+
+#fatiar com um passo negativo para inverter uma substring
+print(texto[::-1])
